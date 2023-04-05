6759ea50ce1f13b52fc97d44db254d778def9b07
dfsalam <dfsalam@gmail.com>
2023-04-05 18:39:13 -0500

    Fix linter errors

--- Gemfile
+++ Gemfile
@@ -1,10 +1,3 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
-
-# Specify your gem's dependencies in morse-code.gemspec
-gemspec
-
-gem "rake", "~> 13.0"
-
 gem 'rubocop', '>= 1.0', '< 2.0'
--- main.rb
+++ main.rb
@@ -1,34 +1,35 @@
-def character(x)
-  arraySymbols=['.-','-...','-.-.','-..','.','..-.','--.','....','..',
-  '.---','-.-.','.-..','--','-.','---','.--.','--.-','.-.','...','-','..-',
-  '...-','.--','-..-','-.--','--..']
-  arrayLetters=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-  counter=0
-  arraySymbols.each { |n|
-    
-    if(n===x)
-      return arrayLetters[counter]
-    end
-    counter = counter + 1    
-    }
+# frozen_string_literal: true
+
+def character(char)
+  array_symbols = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..',
+                   '.---', '-.-.', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-',
+                   '...-', '.--', '-..-', '-.--', '--..']
+  array_letters = %w[A B C D E F G H I J K L M N O P Q R S T
+                     U V W X Y Z]
+  counter = 0
+  array_symbols.each do |n|
+    return array_letters[counter] if n == char
+
+    counter += 1
+  end
 end
 
-def word(x)
-  arr = x.split
+def word(string)
+  arr = string.split
   result = ''
-  arr.each{
-    |letter|
-    result += character(letter);
-  }
-  return result
+  arr.each do |letter|
+    result += character(letter)
+  end
+  result
 end
 
-def phrase(x)
-  arrWords = x.split('   ', -1)
+def phrase(phrase)
+  arr_words = phrase.split('   ', -1)
   result = ''
-  arrWords.each{
-    |element|
-    result += word(element) + " ";
-  }
-  return result
+  arr_words.each do |element|
+    result += "#{word(element)} "
+  end
+  result
 end
+
+puts(phrase('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'))
